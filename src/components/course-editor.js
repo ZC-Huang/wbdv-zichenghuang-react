6319055937a88f0264957333e4b8c8b871766633
ZC-Huang <60633966+ZC-Huang@users.noreply.github.com>
2021-03-10 17:12:52 -0800

    Update for A4

--- src/components/course-editor.js
+++ src/components/course-editor.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import moduleReducer from "../reducers/modules-reducer";
+import lessonReducer from "../reducers/lesson-reducer";
+import {combineReducers, createStore} from "redux";
+import {Provider} from "react-redux";
+import ModuleList from "./module-list";
+import LessonTabs from "./lesson-tabs";
 
-// const CourseEditor = ({props}) =>
-const CourseEditor = ({history}) =>
-  <div>
-    <h2>
-        <Link to="/courses/table">
-            <i className="fas fa-arrow-left"></i>
-        </Link>
-        Course Editor
-        <i onClick={() => history.goBack()}
-           className="fas fa-times float-right"></i>
-    </h2>
-  </div>
+const reducer = combineReducers({
+    moduleReducer: moduleReducer,
+    lessonReducer: lessonReducer
+})
+
+const store = createStore(reducer)
+
+const CourseEditor = ({history}) => {
+    const {courseId, moduleId} = useParams();
+    return (
+    <Provider store={store}>
+        <div>
+            <h2>
+                <Link to="/courses/table">
+                    <i className="fas fa-arrow-left"></i>
+                </Link>
+                Course Editor {courseId} {moduleId}
+                <i onClick={() => history.goBack()}
+                   className="fas fa-times float-right"></i>
+            </h2>
+            <div className="row">
+                <div className="col-4">
+                    <ModuleList/>
+                </div>
+                <div className="col-8">
+                    <LessonTabs/>
+                </div>
+            </div>
+        </div>
+    </Provider>)}
 
 export default CourseEditor
